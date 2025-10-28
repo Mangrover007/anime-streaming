729e37c339596d9f2475b6d2b3013e12057f3b22
MintPotato <pressetouseintellingence@gmail.com>
2025-10-28 08:20:24 +0530

    removed deleted file imported

--- backend/src/routes/admin.mjs
+++ backend/src/routes/admin.mjs
@@ -3,7 +3,6 @@ import { isAdmin } from "../middlewares/isAdmin.mjs";
 import { animeRoute } from "./admin/anime.mjs";
 import { seasonRoute } from "./admin/season.mjs";
 import { episodeRoute } from "./admin/episode.mjs";
-import { genreRoute } from "./admin/genre.mjs";
 import { verifyToken } from "../middlewares/verifyToken.mjs";
 import { commentRoute } from "./admin/comment.mjs";
 
@@ -18,6 +17,5 @@ router.use("/anime", animeRoute);
 router.use("/season", seasonRoute);
 router.use("/episode", episodeRoute);
 router.use("/comment", commentRoute);
-router.use("/genre", genreRoute);
 
 export { router as adminRoute };
