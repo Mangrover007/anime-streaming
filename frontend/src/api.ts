9cf00a35a0c31458141d79546a521a71986e5983
Mangrover007 <Mangrover@proton.me>
2025-11-04 00:53:47 +0530

    frontend deploy version PRE ALPHA V4

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,21 +2,21 @@ import axios from "axios";
 const _baseURL = import.meta.env.BASE_URL;
 
 export const COMMON_URL = axios.create({
-    baseURL: _baseURL,
+    baseURL: `${_baseURL}`,
     withCredentials: true,
 });
 
 export const AUTH_URL = axios.create({
-    baseURL: _baseURL + "auth",
+    baseURL: `${_baseURL}/auth`,
     withCredentials: true,
 });
 
 export const PROT_URL = axios.create({
-    baseURL: _baseURL + "prot",
+    baseURL: `${_baseURL}/prot`,
     withCredentials: true,
 });
 
 export const ADMIN_URL = axios.create({
-    baseURL: _baseURL + "admin",
+    baseURL: `${_baseURL}/admin`,
     withCredentials: true,
 });
